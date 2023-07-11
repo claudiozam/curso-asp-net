2209d709ab393562638e19e5e0e9ba6fd85c9e25
claudio z <claudio.zamoszczyk@gmail.com>
2023-07-11 13:46:38 -0300

    primer ejemplo de validacion

--- Models/ViewModels/ReclamoViewModel.cs
+++ Models/ViewModels/ReclamoViewModel.cs
@@ -1,4 +1,6 @@
-﻿namespace WebApplicationSistemaDeReclamos.Models.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationSistemaDeReclamos.Models.ViewModels
 {
     public class ReclamoViewModel
     {
@@ -9,7 +11,12 @@
         private DateTime fechaAlta;
 
         public long Id { get => id; set => id = value; }
+
+        [Required]
         public string Titulo { get => titulo; set => titulo = value; }
+
+        [Required]
+        [StringLength(100)]
         public string Descripcion { get => descripcion; set => descripcion = value; }
         public string Estado { get => estado; set => estado = value; }
         public DateTime FechaAlta { get => fechaAlta; set => fechaAlta = value; }
